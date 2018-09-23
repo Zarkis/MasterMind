94f57121789c78f1489ad54684ce452eafb397e8
Zarkis <labbeyalexis@gmail.com>
2018-09-23 11:49:30 +0200

    Version0.0.0
    Debut Master Mind

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Master Mind 0.0.0</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<header>
+	<h1 style="text-align: center">Bienvenu à vous monsieur Mj</h1>
+	<h2 style="text-align: center">Que puis je faire pour votre service ?</h2>
+</header>
+<body>
---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+    include('header.php');
+?>
+
+<div class="indexblock">
+
+<h2 style="text-align: center">Notre but :</h2>
+
+<p style="text-align: center">Master Mind fut crée en 2018 par le maitre du jeu Zarkis afin de facilité la gestion de données de jdr telle item, personnage, monstre, boss ... Le but premier de master mind est de faciliter la vie de tout les mj en proposant divers outils personnalisable augmentant ainsi la rapidité des sessions.</p>
+	
+</div>
+
+<div class="indexblock">
+
+<h2 style="text-align: center">News :</h2>
+
+<p style="text-align: center">Master Mind vient tout juste d'être crée. Nous ne savons pas vraiment par quoi commencer tellement il y a de chose que nous souhaitont intégrer. Avant de commencer notre première version, nous devons tout d'abors fixer les premiers objectifs de ce dernier.</p>
+	
+</div>
+
+<div class="indexblock">
+	
+<h2 style="text-align: center">Objectifs et Avancement :</h2>
+
+<h3>0.0.1 :</h3>
+<ul>
+	<li>Faire la page index (index.php):</li>
+	<ul>
+		<li>html <progress value="100" max="100"></progress></li>
+		<li>css <progress value="0" max="100"></progress></li>
+	</ul>
+	<li>Commencer une barre des taches (header.php):</li>
+	<ul>
+		<li>html <progress value="0" max="100"></progress></li>
+		<li>css <progress value="0" max="100"></progress></li>
+	</ul>
+</ul>
+
+</div>
+
+<div class="indexblock">
+
+<h2 style="text-align: center">Idées outils :</h2>
+
+<ul>
+	<li>Communs</li>
+	<ul>
+		<li>Carte du monde(En temps réel)</li>
+		<ul>
+			<li>Pour Mj</li>
+			<ul>
+				<li>Créé un point (lieux,Personnage ...)</li>
+				<li>Créé une zone (Faction,Monstre, ...(Plusieurs filtres))</li>
+				<li>Modifier un point ou zone(A la souris)</li>
+				<li>Déplacer point ou zone(A la souris)</li>
+				<li>Suprimer un point ou une zone</li>
+			</ul>
+		</ul>
+		<li>Session</li>
+		<ul>
+			<li>Zone de combat</li>
+			<li>Plateaux d'exploration</li>
+		</ul>
+		<li>Fan Art</li>
+	</ul>
+	<li>Pour Mj</li>
+	<ul>
+		<li>Time Line</li>
+		<li>Personnages (Stat, Inventaire ...)</li>
+		<li>Items du jeu</li>
+		<li>Pnj</li>
+		<li>Boss</li>
+		<li>Monstre</li>
+		<li>Playlist</li>
+	</ul>
+	<li>Pour Joueurs</li>
+	<ul>
+		<li>Profils</li>
+		<li>Vos personnages</li>
+		<li>Echange d'item entre joeurs</li>
+		<li>Marchand de la Zone</li>
+		<li>Duel Amicaux</li>
+		<li>Chat Joueurs</li>
+		<li>Chat Groupe</li>
+	</ul>
+
+</ul>
+	
+</div>
+
+<?php
+    include('footer.php');
+?>
